c9d4ecb7d50fd1358eb4abd5f4f17c0b48a93ee2
Alex King <alex@crowdfavorite.com>
2010-02-03 02:05:08 +0000

    don't use deprecated functions

--- header/header-default.php
+++ header/header-default.php
@@ -27,7 +27,7 @@ $blog_desc = get_bloginfo('description');
 <head profile="http://gmpg.org/xfn/11">
 	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 
-	<title><?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ).$title_description; ?></title>
+	<title><?php wp_title( '-', true, 'right' ); // echo wp_specialchars( get_bloginfo('name'), 1 ).$title_description; ?></title>
 
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'carrington-text' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'carrington-text' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
