26a747b3aecf49b26f9f953495bc7df3a3106896
Gordon Brander <gordon@crowdfavorite.com>
2011-06-22 15:35:05 +0000

    Removed $previousday access. Replaced the_date() with formatted the_time calls.

--- content/page.php
+++ content/page.php
@@ -17,10 +17,6 @@
 
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 if (CFCT_DEBUG) { cfct_banner(__FILE__); }
-
-global $previousday;
-$previousday = -1;
-
 ?>
 <div id="post-content-<?php the_ID() ?>" <?php post_class('hentry full') ?>>
 	<h1 class="entry-title full-title"><a href="<?php the_permalink() ?>" title="Permanent link to <?php the_title_attribute() ?>" rel="bookmark" rev="post-<?php the_ID(); ?>"><?php the_title() ?></a></h1>
@@ -38,6 +34,6 @@ $previousday = -1;
 	<div class="clear"></div>
 	<div class="by-line">
 		<?php edit_post_link(__('Edit', 'carrington-text'), '<div class="entry-editlink">', '</div>'); ?>
-		<span class="date full-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i'); ?>"><?php the_date(); _e(' at ', 'carrington-text'); the_time(); ?></abbr></span>
+		<span class="date full-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i'); ?>"><?php the_time('F j, Y'); _e(' at ', 'carrington-text'); the_time(); ?></abbr></span>
 	</div><!--/by-line-->
 </div><!-- .post -->
