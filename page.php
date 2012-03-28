e7a6c2bee9a9f0c7e54b875b239cc5cb1fc6fad6
Administrator <Administrator@Sycamore.local>
2012-03-28 11:21:31 -0600

    Removed Pages subdirectory

--- page.php
+++ page.php
@@ -15,9 +15,28 @@
 // **********************************************************************
 
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
-// NOTE: this file is here for compatibility reasons - active templates are in the pages/ dir 
+get_header();
 
-cfct_page();
+?>
+
+<div id="content">
+
+<?php
+
+cfct_loop();
+
+comments_template();
+
+?>
+
+</div>
+
+<?php 
+
+get_sidebar();
+
+get_footer();
 
 ?>
