c3e98f100bc1764bb3506baa0dfd4805db60fefe
Jared Cornwall <jared@crowdfavorite.com>
2012-04-30 10:41:22 -0600

    Initial config.php setup for build script

---
+++ assets/config.php
@@ -0,0 +1,21 @@
+<?php
+// Navigate to asset root using realpath.
+$abspath = realpath(dirname(__FILE__));
+require_once('asset-build/lib/Bundler.php');
+
+$bundler = Bundler::create($abspath);
+
+$bundle_css = new Bundle('css/build.css');
+$bundle_css
+	->set_bundle_key('bundle-personal')
+	->set_language('css')
+	->add('carrington-text', 'css/carrington-text.css');
+$bundler->push($bundle_css);
+
+$bundle_js = new Bundle('js/build.js');
+$bundle_js
+	->set_bundle_key('bundle-personal')
+	->set_language('javascript')
+	->set_meta('dependencies', array('jquery'))
+	->add('global', 'js/carrington-text.js');
+$bundler->push($bundle_js);
